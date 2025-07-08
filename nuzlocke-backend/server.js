2302c831719bb5deefa2dfa8be2bdd0e5682b49c
Ernie von Sesam <ernie.10c@googlemail.com>
2025-07-08 22:38:15 +0200

    Server.js fix

--- nuzlocke-backend/server.js
+++ nuzlocke-backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 const PORT = 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 };
